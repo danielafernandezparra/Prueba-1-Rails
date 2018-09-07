a697c1a9342b388e87f6cef808542fe918682f6a
Daniela Fernandez <daniela.fernandez7@gmail.com>
2018-09-06 21:30:32 -0300

    Prueba 1 Rails

---
+++ todo_app/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'todos/list'
+  resources :todos
+  get 'todos/:id/completed', to: 'todos#completed', as: 'completed'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
